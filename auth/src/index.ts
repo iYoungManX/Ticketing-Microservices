38a958e74460f11f8069e3ae433ac7091fc47314
iYoungManX <90013928+iYoungManX@users.noreply.github.com>
2022-05-15 18:25:43 -0400

    dahjev

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,7 +1,7 @@
 import mongoose, { ConnectOptions } from "mongoose";
 import { app } from "./app";
 const start = async () => {
-  console.log('Starting up ....')
+  console.log('Starting up ....?????')
   if (!process.env.JWT_KEY) {
     console.log("starting up....");
     throw new Error("JWT_KEY must be defined");
