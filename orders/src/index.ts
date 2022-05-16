6a21584c7276d7807b1fff0e1ec8930bde24e401
iYoungManX <90013928+iYoungManX@users.noreply.github.com>
2022-05-15 20:50:48 -0400

    yaoyao

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -6,6 +6,7 @@ import { TicketUpdatedListener } from "./events/listeners/ticket-updated-listene
 import { ExpirationCompleteListener } from "./events/listeners/expiration-complete-listener";
 import { PaymentCreatedListener } from "./events/listeners/payment-created-listener";
 const start = async () => {
+  console.log('Starting....')
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must be defined");
   }
